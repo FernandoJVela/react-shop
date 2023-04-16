fc31e1ea6eecc6eb2f309cfb820ad548b4e2a580
Fernando J. Vela <fer.velav@gmail.com>
2023-04-15 21:12:05 -0500

    Project started

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+const app = (<App />);
+root.render(app);
